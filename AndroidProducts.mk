e989467af2865c4414257e15ebf20afa8d120063
anupam41 <anupamsamanta41@gmail.com>
2022-04-27 00:49:05 +0530

    sweet: YAAP bringup
    Signed-off-by: anupam41 <anupamsamanta41@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The yaapOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/yaap_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sweet-user \
-    lineage_sweet-userdebug \
-    lineage_sweet-eng
+    yaap_sweet-user \
+    yaap_sweet-userdebug \
+    yaap_sweet-eng
